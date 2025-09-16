51c154ed2c42e22cb041ea5aeb07afbd43fa4e0f
Oleg <luzeninoleg2012@gmail.com>
2025-09-16 12:33:26 +0300

    tab structure for the new app and a theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,31 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="leaf" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colorGreen: "#29b365",
+  colorRed: "#FF0000",
+  colorBlue: "#0000FF",
+  colorYellow: "#FFFF00",
+  colorPurple: "#800080",
+  colorWhite: "#fff",
+};
