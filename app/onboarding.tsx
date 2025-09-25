c1954b0e0b3df5e3179c356371359e1f689428cf
Oleg <luzeninoleg2012@gmail.com>
2025-09-25 19:39:59 +0300

    splash screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -49,6 +49,7 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   tagline: {
+    paddingHorizontal: 12,
     fontSize: 24,
     color: theme.colorWhite,
     textAlign: "center"
